4aa3fa762b30192bfe58bd21018ee2876c4ab9ce
saurav <sauravdhr@gmail.com>
2019-11-02 17:35:01 -0400

    stop and clean phyloscanner 50

--- check_progress.py
+++ check_progress.py
@@ -7,13 +7,17 @@ def check_progress():
 	total = len(folders)
 
 	for folder in folders:
-		check_folder = 'outputs/' + folder + '/phyloscanner_output_50_bootstrap/favites_hostRelationshipSummary.csv'
+		check_folder = 'outputs/' + folder + '/phyloscanner_output_50_bootstrap'
 		# RAxML_bestTree = data_dir + folder + '/RAxML_output/RAxML_bestTree.favites'
 		# check_folder = 'outputs/' + folder + '/tnet_best_tree/bestTree.100.tnet_new'
 		# check_folder = data_dir + folder + '/RAxML_output/'
 		if os.path.exists(check_folder):
 			# file_list = next(os.walk(check_folder))[2]
+			check_file = 'outputs/' + folder + '/phyloscanner_output_50_bootstrap/favites_hostRelationshipSummary.csv'
 			count += 1
+			if not os.path.exists(check_file):
+				print(folder)
+				# shutil.remove(check_folder)
 			# count += len(file_list)
 		# else:
 		# 	print(folder)
